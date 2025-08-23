d950c70c615def87f96ad885db928237844f3ffb
Peter Lahanas <plahanas18@gmail.com>
2025-08-23 18:51:58 +1000

    add: infra for etl

---
+++ etls/etl.py
@@ -0,0 +1,32 @@
+import fuelpricesqld.api as fa
+import fuelpricesqld.etls as etls
+from fuelpricesqld.database.session import make_db_engine
+from sqlalchemy.orm import Session
+
+from settings import Settings
+
+
+def etl():
+    settings = Settings()  # type: ignore
+
+    # fuepricesqld api client
+    api_token = settings.FUEL_PRICES_QLD_API_TOKEN
+    api_client = fa.Client(api_token)
+
+    # Connect to DB
+    db_connection_string = settings.PSQL_DB_CONNECTION_STRING
+    engine = make_db_engine(db_connection_string)
+
+    with Session(engine) as session:
+        # Order of these does matter due to foreign key constraints
+        # Put sites after brands & ensure countries table is seeded
+        etls.etl_brands(session, api_client)
+        etls.etl_fuels(session, api_client)
+        etls.etl_regions(session, api_client)
+        etls.etl_sites(session, api_client)
+        etls.etl_prices(session, api_client)
+        session.commit()
+
+
+if __name__ == "__main__":
+    etl()
